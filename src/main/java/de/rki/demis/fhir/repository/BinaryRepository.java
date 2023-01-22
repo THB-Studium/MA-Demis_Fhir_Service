52c8c321a76d4cfd241c1e37b9f76ec948bc77ad
steve ngalamo <steve.ngalamo@gematik.de>
2023-01-22 20:48:01 +0100

    timeScale db connected

--- src/main/java/de/rki/demis/fhir/repository/BinaryRepository.java
+++ src/main/java/de/rki/demis/fhir/repository/BinaryRepository.java
@@ -5,6 +5,7 @@ import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BinaryRepository
